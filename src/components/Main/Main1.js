21cbd0d807111eb21ff5453a943f9fbe03885b48
Kahmyl <59609015+Kahmyl@users.noreply.github.com>
2021-11-07 12:46:11 +0100

    added

--- src/components/Main/Main1.js
+++ src/components/Main/Main1.js
@@ -4,27 +4,27 @@ import './Main1.css';
 import kamil from './Kamil.jpeg'
 const Main1 = () => {
     return(
-        <main class="l-main">
-            <section class="home bd-grid" id="home">
-                <div className="home__data">
-                <Element name="Home" className="element"><h1 className="home__title">Hi,<br/>I'am <span className="home__title-color">Kamil</span><br/>Software developer</h1></Element>
-                    <Element name="About" className="element"><a href="#" className="button">Learn more</a></Element>
-                </div>
-                <div class="home__social">
-                    <a className="SocialIcons" href="https://google.com">
-                    <AiFillGithub size="2.5rem" />
-                    </a>
-                    <a className="SocialIcons" href="https://google.com">
-                        <AiFillInstagram size="2.5rem" />
-                    </a>
-                    <a className="SocialIcons" href="https://google.com">
-                        <AiFillLinkedin size="2.5rem" />
-                    </a>
-                </div>
-                <div className="home__img">
-                    <div className="lease">  
-                    <img className="round" src={kamil} alt=""/>
+        <main class="l-main homedrop">
+            <section className="home-flex"  id="home">
+                <div class="home bd-grid">
+                    <div className="home__data">
+                    <Element name="Home" className="element"><h1 className="home__title">Hi,<br/>I'am <span className="home__title-color">Kamil</span><br/>Software developer</h1></Element>
+                        <Element name="About" className="element"><a href="#" className="button">Learn more</a></Element>
                     </div>
+                    <div class="home__social">
+                        <a className="SocialIcons" href="https://google.com">
+                        <AiFillGithub size="2.5rem" />
+                        </a>
+                        <a className="SocialIcons" href="https://google.com">
+                            <AiFillInstagram size="2.5rem" />
+                        </a>
+                        <a className="SocialIcons" href="https://google.com">
+                            <AiFillLinkedin size="2.5rem" />
+                        </a>
+                    </div>
+                </div>
+                <div className="home__img"> 
+                    <img className="round" src={kamil} alt=""/>
                 </div>
             </section>
         </main>
